49879f7b0e89b5a447ca4e5e6e3a0bbc258d87f5
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2023-03-15 09:54:56 -0700

    search for pdf error (Akena 2012) and upload pdf data and try term frequency function

---
+++ R/get_pdf.R
@@ -0,0 +1,22 @@
+# read pdf documents
+
+# libraries
+library(pdftools)
+library(tm)
+
+files <- list.files(pattern = "pdf$") # creates vector of pdf file names
+optnions <- lapply(files, pdf_text) # loads files
+lapply(optnions, length) # check
+
+pdfdatabase <- Corpus(URISource(files), readerControl = list(reader = readPDF)) # creates pdf database
+
+# term document matrix
+# only words that appear at least 3 times
+optnions_tdm <- TermDocumentMatrix(pdfdatabase, control = list(removePunctuation = TRUE,
+                                                               stopwords = TRUE,
+                                                               tolower = TRUE,
+                                                               stemming = FALSE,
+                                                               removeNumbers = TRUE,
+                                                               bounds = list(global = c(3, Inf))))
+inspect(optnions_tdm[1:10,]) # by document
+findFreqTerms(optnions_tdm, lowfreq = 50, highfreq = Inf) # across all documents
