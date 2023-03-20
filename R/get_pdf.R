738141135e6a649968bedf28c3529263c749198b
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2023-03-20 12:08:43 -0700

    create text data

--- R/get_pdf.R
+++ R/get_pdf.R
@@ -4,11 +4,14 @@
 library(pdftools)
 library(tm)
 
-files <- list.files(pattern = "pdf$") # creates vector of pdf file names
-optnions <- lapply(files, pdf_text) # loads files
-lapply(optnions, length) # check
+dir <- "Data/00_Final lit review" # names location of pdf files
+files <- list.files(dir, pattern = "*.pdf$", full.names = TRUE) # creates vector of pdf file names
+saveRDS(files, file = "list-of-pdfs.rds")
 
-pdfdaabase <- Corpus(URISource(files), readerControl = list(reader = readPDF)) # creates pdf database
+optnions <- lapply(files, pdf_text) # loads files
+lapply(optnions, length) # check number of pdf and pages
+
+pdfdatabase <- Corpus(URISource(files), readerControl = list(reader = readPDF)) # creates pdf database
 
 # term document matrix
 # only words that appear at least 3 times
@@ -19,4 +22,4 @@ optnions_tdm <- TermDocumentMatrix(pdfdatabase, control = list(removePunctuation
                                                                removeNumbers = TRUE,
                                                                bounds = list(global = c(3, Inf))))
 inspect(optnions_tdm[1:10,]) # by document
-findFreqTerms(optnions_tdm, lowfreq = 75, highfreq = Inf) # across all documents
+findFreqTerms(optnions_tdm, lowfreq = 150, highfreq = Inf) # across all documents
---
+++ R/get_text.R
@@ -0,0 +1,64 @@
+# create text data, prepare to use/test for tidytext package
+
+# library
+library(tidytext)
+library(readtext)
+library(ggplot2)
+library(ggthemes)
+library(textdata)
+
+# get data (in Plain Text UTF-8)
+tidy_pdf <- readtext(files)
+
+tidy_words <- tidy_pdf %>%
+  unnest_tokens(word, text)
+
+# try some stuff...
+# Which words appear most frequently and how many times do they each appear?
+quetion_1 <- tidy_words %>%
+  count(word, sort = TRUE)
+
+quetion_1 <- tidy_words %>%
+  anti_join(stop_words) %>% # without stop words
+  count(word, sort = TRUE)
+
+# How many words appear more than 1000 times?
+quetion_1 %>%
+  filter(n > 1000) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkred") +
+  theme_fivethirtyeight() +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Word Usage")
+
+# What sentiment score is assigned to “fear”?
+get_sentiments("nrc")
+#nrc categorizes words as positive, negative, anger, anticipation, disgust, fear, joy, sadness, surprise and trust.
+get_sentiments("bing")
+#bing categorizes words as positive or negative.
+get_sentiments("afinn")
+#AFINN assigns words a numeric score between -5 and 5, where the negative scores indicate negative sentiment and positive scores indicate positive sentiment.
+
+sentiment_bing <- quetion_1 %>%
+  inner_join(get_sentiments("bing"))
+
+sentiment_bing %>%
+  filter(n > 100) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill=sentiment)) +
+  theme_fivethirtyeight() +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ylab("Word Count") +
+  ggtitle("Word Usage", subtitle = "Sentiment Analysis Using
+Bing et al.")
+
+nrc_fear <- get_sentiments("nrc") %>%
+  filter(sentiment == "fear")
+
+fear_words <- quetion_1 %>%
+  inner_join(nrc_fear)
