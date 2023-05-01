8c49fdce3cb62af9e4d01d6811cae99506d42002
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2023-05-01 14:10:46 -0700

    update key, removal of pdfs that were not read by the group

--- R/make_synonym.R
+++ R/make_synonym.R
@@ -9,12 +9,21 @@ text_data <- read.csv("Data/clean_text_data.csv")
 head(text_data)
 str(text_data)
 
+# remove pdfs without metadata (not read by group)
+meta_data <- read.csv("Data/clean_meta_data.csv")
+head(meta_data)
+str(meta_data)
+
+ID <- meta_data[,c(1,33)]
+reduce_by_ID <- merge(ID, text_data, by = "doc_id", all = TRUE)
+reduce_by_ID <- na.omit(reduce_by_ID)
+
 # simplify to unique pairings of words and stems
-make_key <- unique(text_data[,c(2,3)])
+make_key <- unique(reduce_by_ID[,c(3,4)])
 head(make_key)
 
 # add frequency of words in data
-word_count <- text_data %>%
+word_count <- reduce_by_ID %>%
   count(word)
 
 make_key_w_count <- merge(make_key, word_count, by = "word", all = TRUE)
